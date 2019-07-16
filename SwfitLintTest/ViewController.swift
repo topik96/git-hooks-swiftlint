c892dfa32281d4d3dcd65d19f21e0f3730765d49
Topik Mujianto <topikmujianto7@gmail.com>
2019-07-16 13:06:54 +0700

    Initial Commit

---
+++ SwfitLintTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwfitLintTest
+//
+//  Created by Topik Mujianto on 16/07/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
